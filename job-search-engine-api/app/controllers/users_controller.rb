b941f43864d3ef0184e31f8be9f337f98939457d
Edward Castillo <castilloedwarda@gmail.com>
2020-02-07 13:02:15 -0500

    crud user and working webscrape

--- job-search-engine-api/app/controllers/users_controller.rb
+++ job-search-engine-api/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def new 
+        user = User.new
+    end
+
+    def create 
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
+
+    def edit
+        user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :location)
+    end
 end
--- job-search-engine-api/db/seeds.rb
+++ job-search-engine-api/db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+require 'rest-client'
+
 doc = RestClient.get('https://www.meetup.com/topics/')
 body = doc.body
 main = body.split('<a name="Tech">')[1].split('<a name="Writing"')[0]
