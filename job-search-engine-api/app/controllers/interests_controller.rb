2f8c86f7b182a9b1cc49b5bfb9534d832f85def1
Edward Castillo <castilloedwarda@gmail.com>
2020-02-07 13:43:50 -0500

    topics webscrape officially working

--- job-search-engine-api/app/controllers/interests_controller.rb
+++ job-search-engine-api/app/controllers/interests_controller.rb
@@ -1,2 +1,11 @@
 class InterestsController < ApplicationController
+    def index
+        interests = Interest.all 
+        render json: interests
+    end
+
+    def show
+        interest = Interest.find(params[:id])
+        render json: interest
+    end
 end
